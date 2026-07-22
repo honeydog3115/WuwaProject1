eed64f605161202c75bd0f09fd94da7cea9e8253
honeydog3115 <tjs91002256@gmail.com>
2026-07-22 20:06:43 +0900

    create project

---
+++ src/main/java/com/sjb/wuwaechorank/WuwaEchorankApplication.java
@@ -0,0 +1,13 @@
+package com.sjb.wuwaechorank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WuwaEchorankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WuwaEchorankApplication.class, args);
+	}
+
+}
