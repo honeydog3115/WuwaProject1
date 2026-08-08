bdb9ff4d29388bd40f49d06d40019c71399b8dc4
honeydog3115 <tjs91002256@gmail.com>
2026-08-08 20:06:16 +0900

    add: ResonatorService.getAll() 전체 공명자 정보 반환, ResonatorService.getValidStat() 공명자 유효 능력치 반환

--- src/main/java/com/sjb/wuwaechorank/service/resonator/ResonatorServiceImpl.java
+++ src/main/java/com/sjb/wuwaechorank/service/resonator/ResonatorServiceImpl.java
@@ -1,9 +1,14 @@
 package com.sjb.wuwaechorank.service.resonator;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.sjb.wuwaechorank.dao.entity.resonator.ResonatorDao;
+import com.sjb.wuwaechorank.entity.Resonator;
 
+
+// 공명자 서비스
 @Service
 public class ResonatorServiceImpl implements ResonatorService {
     private ResonatorDao resonatorDao;
@@ -12,5 +17,13 @@ public class ResonatorServiceImpl implements ResonatorService {
         this.resonatorDao = resonatorDao;
     }
 
+    // 전체 공명자 정보 반환
+    public List<Resonator> getAll(){
+        return resonatorDao.getAll();
+    }
 
+    // 공명자의 유효 능력치 반환.
+    public void getValidStat(){
+
+    }
 }
