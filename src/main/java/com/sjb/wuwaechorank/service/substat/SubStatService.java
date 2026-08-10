fadd2be2f424e650b885eed6db4c1b58594a879a
honeydog3115 <tjs91002256@gmail.com>
2026-08-10 19:59:23 +0900

    add: 부음속성 관련 서비스 인터페이스와 구현 클래스 생성, getSubStat() 에코의 부음속성 선택에 필요한 모든 부음속성의 정보를 반환하는 함수.

---
+++ src/main/java/com/sjb/wuwaechorank/service/substat/SubStatService.java
@@ -0,0 +1,10 @@
+package com.sjb.wuwaechorank.service.substat;
+
+import java.util.List;
+
+import com.sjb.wuwaechorank.dto.SubStatDto;
+
+// 부음속성 관련    서비스 인터페이스
+public interface SubStatService {
+    List<SubStatDto> getSubStat();
+}
