837f5f984f8071ee23384167569ba3adb6c8da5e
honeydog3115 <tjs91002256@gmail.com>
2026-08-10 14:24:22 +0900

    add: 화음효과 id를 기준으로 echo 엔티티를 가져오는 SQL 작성

--- src/main/java/com/sjb/wuwaechorank/dao/entity/echo/EchoDaoCoreJDBC.java
+++ src/main/java/com/sjb/wuwaechorank/dao/entity/echo/EchoDaoCoreJDBC.java
@@ -1,13 +1,26 @@
 package com.sjb.wuwaechorank.dao.entity.echo;
 
+import java.util.List;
+
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+
+import com.sjb.wuwaechorank.entity.Echo;
 
 @Repository
 public class EchoDaoCoreJDBC implements EchoDaoCore {
     private final JdbcTemplate jdbcTemplate;
+    private RowMapper<Echo> rowMapper = BeanPropertyRowMapper.newInstance(Echo.class);
 
     public EchoDaoCoreJDBC(JdbcTemplate jdbcTemplate){
         this.jdbcTemplate = jdbcTemplate;
     }
+
+    @Override
+    public List<Echo> getAllEchoBySonataEffect(int id) {
+        return this.jdbcTemplate.query("SELECT * FROM echo WHERE sonataEffectId = ?", rowMapper, id);
+    }
+    
 }
