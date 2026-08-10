fa15cca4a98ba5a8374a96c88cd7e5d2bcad7239
honeydog3115 <tjs91002256@gmail.com>
2026-08-10 15:27:32 +0900

    update: 클래스 구조를 record builder 패턴으로 변경. setter는 제거

--- src/main/java/com/sjb/wuwaechorank/dto/EchoInfoGroupBySonataEffectDto.java
+++ src/main/java/com/sjb/wuwaechorank/dto/EchoInfoGroupBySonataEffectDto.java
@@ -3,21 +3,18 @@ package com.sjb.wuwaechorank.dto;
 import java.util.List;
 
 import com.sjb.wuwaechorank.entity.Echo;
-import com.sjb.wuwaechorank.entity.SonataEffect;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 // 화음 이펙트로 그룹화한 에코 정보 DTO
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
-public class EchoInfoGroupBySonataEffectDto {
-    int id;
-    String name;
-    String imagePath;
-    List<Echo> echos;
-}
+@Builder
+public record EchoInfoGroupBySonataEffectDto(
+    int id,
+    String name,
+    String imagePath,
+    List<Echo> echos
+){}
