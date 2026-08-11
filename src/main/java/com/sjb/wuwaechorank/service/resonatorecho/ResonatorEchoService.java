5015944e36dd05564c6fa5b469984f38918976f1
honeydog3115 <tjs91002256@gmail.com>
2026-08-11 16:43:28 +0900

    add: ResonatorEchoService에 공명자 에코 총 점수를 계산하는 getResonatorEchooScore() 함수 추가

--- src/main/java/com/sjb/wuwaechorank/service/resonatorecho/ResonatorEchoService.java
+++ src/main/java/com/sjb/wuwaechorank/service/resonatorecho/ResonatorEchoService.java
@@ -1,6 +1,11 @@
 package com.sjb.wuwaechorank.service.resonatorecho;
 
+import java.util.List;
+
+import com.sjb.wuwaechorank.dto.ResonatorEchoInfoDto;
+
 // 공명자의 에코와 관련된 서비스 인터페이스
 public interface ResonatorEchoService {
-    
+    // 공명자가 착용한 에코들의 점수를 구해서 총 점수를 반환하는 함수.
+    double getResonatorEchoScore(List<ResonatorEchoInfoDto> resonatorEchosInfo);
 }
