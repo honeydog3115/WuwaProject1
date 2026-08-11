63e72299b881752d45d6d1365d74f2aee1e6d13a
honeydog3115 <tjs91002256@gmail.com>
2026-08-11 16:45:44 +0900

    add: getResonatorEchoScore()구현체 작성, 해당 함수 구현에 필요한 clacEchoScore, clacSubStatScore 함수 추가

--- src/main/java/com/sjb/wuwaechorank/service/resonatorecho/ResonatorEchoServiceImpl.java
+++ src/main/java/com/sjb/wuwaechorank/service/resonatorecho/ResonatorEchoServiceImpl.java
@@ -1,17 +1,50 @@
 package com.sjb.wuwaechorank.service.resonatorecho;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.sjb.wuwaechorank.dao.entity.resonatorecho.ResonatorEchoDao;
+import com.sjb.wuwaechorank.dao.entity.substatinfo.SubStatInfoDao;
+import com.sjb.wuwaechorank.dto.ResonatorEchoInfoDto;
+import com.sjb.wuwaechorank.dto.ResonatorEchoSubStatDto;
+import com.sjb.wuwaechorank.entity.SubStatInfo;
 
 //공명자 에코와 관련된 서비스 구현 클래스
 @Service
 public class ResonatorEchoServiceImpl implements ResonatorEchoService{
+    private static final double MAX_SUBSTAT_SCORE = 20;
     private ResonatorEchoDao resonatorEchoDao;
+    private SubStatInfoDao subStatInfoDao;
     
-    public ResonatorEchoServiceImpl(ResonatorEchoDao resonatorEchoDao){
+    public ResonatorEchoServiceImpl(ResonatorEchoDao resonatorEchoDao, SubStatInfoDao subStatInfoDao){
         this.resonatorEchoDao = resonatorEchoDao;
+        this.subStatInfoDao = subStatInfoDao;
     }
 
-    
+    @Override
+    public double getResonatorEchoScore(List<ResonatorEchoInfoDto> resonatorEchosInfo) {
+        // TODO Auto-generated method stub
+        return resonatorEchosInfo.stream().map(this::clacEchoScore).mapToDouble(Double::doubleValue).average().getAsDouble();
+    }
+
+    // 예코 점수 계산 함수
+    private double clacEchoScore(ResonatorEchoInfoDto resonatorEchoInfoDto){
+        double score = resonatorEchoInfoDto.echoSubStats().stream()
+                .map(this::clacSubStatScore)
+                .mapToDouble(Double::doubleValue)
+                .sum();
+        
+        return score;
+    }
+
+    // 부음속성 점수 계산함수
+    private double clacSubStatScore(ResonatorEchoSubStatDto echoSubStat){
+        List<String> values = this.subStatInfoDao.getAllBySubStatId(echoSubStat.subStatId()).stream().map(subStatinfo->subStatinfo.getValue()).toList();
+        // 기본 점수 단위는 스탯당 최대점수(20)를 옵션 수로 나눈값
+        double baseScoreUnit = MAX_SUBSTAT_SCORE/values.size();
+        // 가중치는 해당 옵션이 상옵일 수록 올라감.
+        int weight = values.indexOf(echoSubStat.value())+1;
+        return baseScoreUnit * weight;
+    }
 }
