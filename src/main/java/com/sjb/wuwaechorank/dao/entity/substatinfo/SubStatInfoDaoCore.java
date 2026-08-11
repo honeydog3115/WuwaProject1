d799ecddab6cc4de0b5c57176db0890859dfe724
honeydog3115 <tjs91002256@gmail.com>
2026-08-11 14:47:06 +0900

    add: subStatId를 기준으로 SubStatInfo들을 가져오는 getAllBySubStatId() 작성

--- src/main/java/com/sjb/wuwaechorank/dao/entity/substatinfo/SubStatInfoDaoCore.java
+++ src/main/java/com/sjb/wuwaechorank/dao/entity/substatinfo/SubStatInfoDaoCore.java
@@ -1,8 +1,12 @@
 package com.sjb.wuwaechorank.dao.entity.substatinfo;
 
+import java.util.List;
+
 import com.sjb.wuwaechorank.customannotation.DaoCoreInterface;
+import com.sjb.wuwaechorank.entity.SubStatInfo;
 
 @DaoCoreInterface
 public interface SubStatInfoDaoCore {
-    
+    // subStatId를 기준으로 SubStatInfo 엔티티를 전부 가져옴.
+    List<SubStatInfo> getAllBySubStatId(int subStatId);
 }
--- src/main/java/com/sjb/wuwaechorank/dao/entity/substatinfo/SubStatInfoDaoCoreJDBC.java
+++ src/main/java/com/sjb/wuwaechorank/dao/entity/substatinfo/SubStatInfoDaoCoreJDBC.java
@@ -1,13 +1,25 @@
 package com.sjb.wuwaechorank.dao.entity.substatinfo;
 
+import java.util.List;
+
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+
+import com.sjb.wuwaechorank.entity.SubStatInfo;
 
 @Repository
 public class SubStatInfoDaoCoreJDBC implements SubStatInfoDaoCore {
     private final JdbcTemplate jdbcTemplate;
+    private RowMapper<SubStatInfo> rowMapper = BeanPropertyRowMapper.newInstance(SubStatInfo.class);
+
     public SubStatInfoDaoCoreJDBC(JdbcTemplate jdbcTemplate){
         this.jdbcTemplate = jdbcTemplate;
     }
     
+    @Override
+    public List<SubStatInfo> getAllBySubStatId(int subStatId) {
+        return this.jdbcTemplate.query("SELECT * FROM substatinfo WHERE ORDER BY value", rowMapper, subStatId);
+    }
 }
