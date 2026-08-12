11d5be72eabce1936147ba8f20929350cee4c9d7
honeydog3115 <tjs91002256@gmail.com>
2026-08-12 13:58:53 +0900

    update: getResonatorEchoScore()의 로직을 수정, getAllBySubStatIdIn()을 사용하게 바꿔서 DB 조회를 줄임. 그에 맞춰 일부 코드 수정

--- src/main/java/com/sjb/wuwaechorank/service/resonatorecho/ResonatorEchoServiceImpl.java
+++ src/main/java/com/sjb/wuwaechorank/service/resonatorecho/ResonatorEchoServiceImpl.java
@@ -1,6 +1,9 @@
 package com.sjb.wuwaechorank.service.resonatorecho;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
@@ -11,6 +14,7 @@ import com.sjb.wuwaechorank.dao.entity.validstat.ValidStatDao;
 import com.sjb.wuwaechorank.dto.ResonatorEchoInfoDto;
 import com.sjb.wuwaechorank.dto.ResonatorEchoSubStatDto;
 import com.sjb.wuwaechorank.entity.ResonatorEcho;
+import com.sjb.wuwaechorank.entity.SubStatInfo;
 import com.sjb.wuwaechorank.entity.ValidStat;
 
 //공명자 에코와 관련된 서비스 구현 클래스
@@ -21,7 +25,6 @@ public class ResonatorEchoServiceImpl implements ResonatorEchoService{
     private SubStatInfoDao subStatInfoDao;
     private ResonatorDao resonatorDao;
     private ValidStatDao validStatDao;
-    private ValidStat validStat;
     
     public ResonatorEchoServiceImpl(ResonatorEchoDao resonatorEchoDao, SubStatInfoDao subStatInfoDao, ResonatorDao resonatorDao, ValidStatDao validStatDao){
         this.resonatorEchoDao = resonatorEchoDao;
@@ -32,34 +35,44 @@ public class ResonatorEchoServiceImpl implements ResonatorEchoService{
 
     @Override
     public double getResonatorEchoScore(int resonatorId, List<ResonatorEchoInfoDto> resonatorEchosInfo) {
-        // TODO Auto-generated method stub
-        this.validStat = this.validStatDao.get(this.resonatorDao.get(resonatorId).getValidStatId());
+        ValidStat validStat = this.validStatDao.get(this.resonatorDao.get(resonatorId).getValidStatId());
+        List<Integer> subStatIds = resonatorEchosInfo.stream()
+                .flatMap(resonatorEchoInfo->resonatorEchoInfo.echoSubStats().stream())
+                .map(ResonatorEchoSubStatDto::subStatId)
+                .distinct()
+                .toList();
+
+        Map<Integer, List<String>> idValueMap = this.subStatInfoDao.getAllBySubStatIdIn(subStatIds).stream()
+                .collect(Collectors.groupingBy(
+                    SubStatInfo::getSubStatId,
+                    Collectors.mapping(SubStatInfo::getValue, Collectors.toList())
+                ));
+
         double totalScore = resonatorEchosInfo.stream()
-                .map(this::clacEchoScore)
+                .map(resonatorEchoInfo->this.calcEchoScore(resonatorEchoInfo, validStat, idValueMap))
                 .mapToDouble(Double::doubleValue)
                 .average()
                 .getAsDouble();
         
-
         return totalScore;
     }
 
     // 예코 점수 계산 함수
-    private double clacEchoScore(ResonatorEchoInfoDto resonatorEchoInfoDto){
+    private double calcEchoScore(ResonatorEchoInfoDto resonatorEchoInfoDto, ValidStat validStat, Map<Integer, List<String>> idValueMap){
         List<ResonatorEchoSubStatDto> echoSubStats = resonatorEchoInfoDto.echoSubStats();
         double score = resonatorEchoInfoDto.echoSubStats().stream()
-                .map(this::clacSubStatScore)
+                .map(resonatorEchoSubStat->this.calcSubStatScore(resonatorEchoSubStat, validStat, idValueMap))
                 .mapToDouble(Double::doubleValue)
                 .sum();
 
         this.resonatorEchoDao.add(
             ResonatorEcho.builder()
                 .echoId(resonatorEchoInfoDto.echoId())
-                .SubStatId1(echoSubStats.get(0).subStatId())
-                .SubStatId2(echoSubStats.get(1).subStatId())
-                .SubStatId3(echoSubStats.get(2).subStatId())
-                .SubStatId4(echoSubStats.get(3).subStatId())
-                .SubStatId5(echoSubStats.get(4).subStatId())
+                .SubStatId1(this.getOrNull(echoSubStats, 0))
+                .SubStatId2(this.getOrNull(echoSubStats, 1))
+                .SubStatId3(this.getOrNull(echoSubStats, 2))
+                .SubStatId4(this.getOrNull(echoSubStats, 3))
+                .SubStatId5(this.getOrNull(echoSubStats, 4))
                 .score(score)
                 .build()
         );
@@ -68,16 +81,20 @@ public class ResonatorEchoServiceImpl implements ResonatorEchoService{
     }
 
     // 부음속성 점수 계산함수
-    private double clacSubStatScore(ResonatorEchoSubStatDto echoSubStat){
+    private double calcSubStatScore(ResonatorEchoSubStatDto echoSubStat, ValidStat validStat, Map<Integer, List<String>> idValueMap){
         if(!validStat.containSubStat(echoSubStat.subStatId()))
             return 0;
         
-        List<String> values = this.subStatInfoDao.getAllBySubStatId(echoSubStat.subStatId()).stream().map(subStatinfo->subStatinfo.getValue()).toList();
+        List<String> values = idValueMap.get(echoSubStat.subStatId());
 
         // 기본 점수 단위는 스탯당 최대점수(20)를 옵션 수로 나눈값
         double baseScoreUnit = MAX_SUBSTAT_SCORE/values.size();
         // 가중치는 해당 옵션이 상옵일 수록 올라감.
         int weight = values.indexOf(echoSubStat.value())+1;
         return baseScoreUnit * weight;
+    }
+
+    private Integer getOrNull(List<ResonatorEchoSubStatDto> echoSubStats, int index){
+        return echoSubStats.size() < index ? null : echoSubStats.get(index).subStatId();
     }
 }
