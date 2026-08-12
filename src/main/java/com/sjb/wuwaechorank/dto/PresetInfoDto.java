bd0a39c687c4d23d786a8f383f36835302b189df
honeydog3115 <tjs91002256@gmail.com>
2026-08-12 14:20:06 +0900

    add: 주석 추가

--- src/main/java/com/sjb/wuwaechorank/dto/PresetInfoDto.java
+++ src/main/java/com/sjb/wuwaechorank/dto/PresetInfoDto.java
@@ -2,6 +2,7 @@ package com.sjb.wuwaechorank.dto;
 
 import java.util.List;
 
+// PresetService.savePreset()에 사용될 DTO
 public record PresetInfoDto(
     String name,
     int resonatorId,
--- src/main/java/com/sjb/wuwaechorank/service/preset/PresetService.java
+++ src/main/java/com/sjb/wuwaechorank/service/preset/PresetService.java
@@ -2,6 +2,8 @@ package com.sjb.wuwaechorank.service.preset;
 
 import com.sjb.wuwaechorank.dto.PresetInfoDto;
 
+// 프리셋 관련 서비스 인터페이스
 public interface PresetService {
+    // 프리셋 저장 함수
     void savePreset(PresetInfoDto presetInfoDto);
 }
