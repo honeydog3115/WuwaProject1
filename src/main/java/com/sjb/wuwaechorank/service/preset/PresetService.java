6915f0a08b300a4b6728f7a4d9de104b978233ed
honeydog3115 <tjs91002256@gmail.com>
2026-08-12 14:18:41 +0900

    add: PresetService 인터페이스와 프리셋 저장 함수인 savePreset() 생성

---
+++ src/main/java/com/sjb/wuwaechorank/service/preset/PresetService.java
@@ -0,0 +1,7 @@
+package com.sjb.wuwaechorank.service.preset;
+
+import com.sjb.wuwaechorank.dto.PresetInfoDto;
+
+public interface PresetService {
+    void savePreset(PresetInfoDto presetInfoDto);
+}
