f8b109f9fd3690772d4099208a2e75ef2960350d
honeydog3115 <tjs91002256@gmail.com>
2026-08-12 19:53:51 +0900

    add: 프리셋을 저장하는 PresetService.savePreset() 추가

---
+++ src/main/java/com/sjb/wuwaechorank/service/preset/PresetServiceImpl.java
@@ -0,0 +1,57 @@
+package com.sjb.wuwaechorank.service.preset;
+
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+
+import com.sjb.wuwaechorank.dao.entity.preset.PresetDao;
+import com.sjb.wuwaechorank.dao.entity.resonatorecho.ResonatorEchoDao;
+import com.sjb.wuwaechorank.dto.PresetInfoDto;
+import com.sjb.wuwaechorank.dto.ResonatorEchoInfoDto;
+import com.sjb.wuwaechorank.dto.ResonatorEchoSubStatDto;
+import com.sjb.wuwaechorank.entity.Preset;
+import com.sjb.wuwaechorank.entity.Resonator;
+import com.sjb.wuwaechorank.entity.ResonatorEcho;
+
+// PresetService 인터페이스 구현체
+@Service
+public class PresetServiceImpl implements PresetService {
+    private PresetDao presetDao;
+    private ResonatorEchoDao resonatorEchoDao;
+
+    public PresetServiceImpl(PresetDao presetDao, ResonatorEchoDao resonatorEchoDao) {
+        this.presetDao = presetDao;
+        this.resonatorEchoDao = resonatorEchoDao;
+    }
+
+    @Override
+    public void savePreset(PresetInfoDto presetInfoDto) {
+        this.presetDao.add(
+                Preset.builder()
+                        .userId(presetInfoDto.userId())
+                        .name(presetInfoDto.name())
+                        .resonatorId(presetInfoDto.resonatorId())
+                        .echoTotalScore(presetInfoDto.score())
+                        .bookmark(false)
+                        .build());
+
+        List<ResonatorEchoInfoDto> resonatorEchoInfos = presetInfoDto.echosInfo();
+        resonatorEchoInfos.stream()
+                .forEach(resonatorEchoInfo -> this.resonatorEchoDao.add(ResonatorEcho.builder()
+                        .echoId(resonatorEchoInfo.echoId())
+                        .SubStatId1(this.getOrNull(resonatorEchoInfo.echoSubStats(), 0))
+                        .SubStatId2(this.getOrNull(resonatorEchoInfo.echoSubStats(), 1))
+                        .SubStatId3(this.getOrNull(resonatorEchoInfo.echoSubStats(), 2))
+                        .SubStatId4(this.getOrNull(resonatorEchoInfo.echoSubStats(), 3))
+                        .SubStatId5(this.getOrNull(resonatorEchoInfo.echoSubStats(), 4))
+                        .build()));
+    }
+
+
+    // 리스트의 범위를 벗어나면 null을 주는 함수
+    private Integer getOrNull(List<ResonatorEchoSubStatDto> echoSubStats, int index) {
+        return echoSubStats.size() < index ? null : echoSubStats.get(index).subStatId();
+    }
+}
