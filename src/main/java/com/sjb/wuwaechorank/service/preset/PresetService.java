ec9d229a9556cf264235bd4db827f7158148e665
honeydog3115 <tjs91002256@gmail.com>
2026-08-12 19:56:11 +0900

    add: 초기에 프리셋 정보를 반환할 getSimplePresetInfo() 추가

--- src/main/java/com/sjb/wuwaechorank/service/preset/PresetService.java
+++ src/main/java/com/sjb/wuwaechorank/service/preset/PresetService.java
@@ -1,9 +1,13 @@
 package com.sjb.wuwaechorank.service.preset;
 
+import java.util.List;
+
 import com.sjb.wuwaechorank.dto.PresetInfoDto;
+import com.sjb.wuwaechorank.dto.SimplePresetInfoDto;
 
 // 프리셋 관련 서비스 인터페이스
 public interface PresetService {
     // 프리셋 저장 함수
     void savePreset(PresetInfoDto presetInfoDto);
+    List<SimplePresetInfoDto> getSimplePresetInfo();
 }
