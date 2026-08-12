480d79b9e4190210f8b2351788247be345deb779
honeydog3115 <tjs91002256@gmail.com>
2026-08-12 10:15:40 +0900

    add: SubStatInfoDaoCore에 substatid 들에 해당하는 substatinfo를 전부 가져오는 getAllBySubStatIdIn() 함수 추가. SubStatInfoDaoCoreJDBC에 함수 구현체 추가

--- src/main/java/com/sjb/wuwaechorank/dao/entity/substatinfo/SubStatInfoDaoCore.java
+++ src/main/java/com/sjb/wuwaechorank/dao/entity/substatinfo/SubStatInfoDaoCore.java
@@ -9,4 +9,6 @@ import com.sjb.wuwaechorank.entity.SubStatInfo;
 public interface SubStatInfoDaoCore {
     // subStatId를 기준으로 SubStatInfo 엔티티를 전부 가져옴.
     List<SubStatInfo> getAllBySubStatId(int subStatId);
+    // subStatIds에 해당하는 SubStatInfo 들을 전부 가져옴.
+    List<SubStatInfo> getAllBySubStatIdIn(List<Integer> subStatIds);
 }
--- src/main/java/com/sjb/wuwaechorank/dao/entity/substatinfo/SubStatInfoDaoCoreJDBC.java
+++ src/main/java/com/sjb/wuwaechorank/dao/entity/substatinfo/SubStatInfoDaoCoreJDBC.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.sjb.wuwaechorank.entity.SubStatInfo;
@@ -12,14 +14,29 @@ import com.sjb.wuwaechorank.entity.SubStatInfo;
 @Repository
 public class SubStatInfoDaoCoreJDBC implements SubStatInfoDaoCore {
     private final JdbcTemplate jdbcTemplate;
+    // IN 구문에 파라미터를 넣으려면 NamedParameterJdbcTemplate이 필요
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
     private RowMapper<SubStatInfo> rowMapper = BeanPropertyRowMapper.newInstance(SubStatInfo.class);
 
-    public SubStatInfoDaoCoreJDBC(JdbcTemplate jdbcTemplate){
+    public SubStatInfoDaoCoreJDBC(JdbcTemplate jdbcTemplate, NamedParameterJdbcTemplate namedParameterJdbcTemplate){
         this.jdbcTemplate = jdbcTemplate;
+        this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
     }
     
     @Override
     public List<SubStatInfo> getAllBySubStatId(int subStatId) {
         return this.jdbcTemplate.query("SELECT * FROM substatinfo WHERE substatid = ? ORDER BY value", rowMapper, subStatId);
     }
+
+    @Override
+    public List<SubStatInfo> getAllBySubStatIdIn(List<Integer> subStatIds) {
+        if(subStatIds == null || subStatIds.isEmpty())
+            return List.of();
+
+        MapSqlParameterSource parameter = new MapSqlParameterSource();
+        parameter.addValue("ids", subStatIds);
+        
+        return this.namedParameterJdbcTemplate.query("SELECT * FROM substatinfo WHERE substatid IN (:ids)", parameter, rowMapper);
+    }
+    
 }
