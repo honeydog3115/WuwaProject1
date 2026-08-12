027970c3522a095f87f89d5a9a02d48547c718e1
honeydog3115 <tjs91002256@gmail.com>
2026-08-12 20:05:06 +0900

    add: userid를 기준으로 preset을 가져오는 getAllByUserId 생성

--- src/main/java/com/sjb/wuwaechorank/dao/entity/preset/PresetDaoCore.java
+++ src/main/java/com/sjb/wuwaechorank/dao/entity/preset/PresetDaoCore.java
@@ -1,8 +1,11 @@
 package com.sjb.wuwaechorank.dao.entity.preset;
 
+import java.util.List;
+
 import com.sjb.wuwaechorank.customannotation.DaoCoreInterface;
+import com.sjb.wuwaechorank.entity.Preset;
 
 @DaoCoreInterface
 public interface PresetDaoCore {
-    
+    List<Preset> getAllByUserId(int userId);
 }
--- src/main/java/com/sjb/wuwaechorank/dao/entity/preset/PresetDaoCoreJDBC.java
+++ src/main/java/com/sjb/wuwaechorank/dao/entity/preset/PresetDaoCoreJDBC.java
@@ -1,8 +1,25 @@
 package com.sjb.wuwaechorank.dao.entity.preset;
 
+import java.util.List;
+
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+
+import com.sjb.wuwaechorank.entity.Preset;
 
 @Repository
 public class PresetDaoCoreJDBC implements PresetDaoCore {
+    private final JdbcTemplate jdbcTemplate;
+    private RowMapper<Preset> rowMapper = BeanPropertyRowMapper.newInstance(Preset.class);
     
+    public PresetDaoCoreJDBC(JdbcTemplate jdbcTemplate){
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public List<Preset> getAllByUserId(int userId){
+        return this.jdbcTemplate.query("SELECT id, name, bookmark FROM preset WHERE userId = ? ", rowMapper, userId);
+    }
 }
