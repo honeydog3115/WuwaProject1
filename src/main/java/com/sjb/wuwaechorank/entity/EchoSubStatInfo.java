a1ff8913b243495f3f75aaea0ab49caee212470e
honeydog3115 <tjs91002256@gmail.com>
2026-08-13 15:29:49 +0900

    add: ResonatorEcho의 부음속성 정보를 담을 EchoSubStatInfo를 생성

---
+++ src/main/java/com/sjb/wuwaechorank/entity/EchoSubStatInfo.java
@@ -0,0 +1,27 @@
+package com.sjb.wuwaechorank.entity;
+
+import com.sjb.wuwaechorank.customannotation.ForeignKey;
+import com.sjb.wuwaechorank.customannotation.PrimaryKey;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class EchoSubStatInfo {
+    @PrimaryKey
+    @Builder.Default
+    int id = 1;
+    @ForeignKey
+    @Builder.Default
+    int resonatorEchoId = 1;
+    @ForeignKey
+    @Builder.Default
+    int substatInfoId = 1;
+}
