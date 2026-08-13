afbfa369f0f1a80d2ad02a24d5a172795a5d8f30
honeydog3115 <tjs91002256@gmail.com>
2026-08-13 10:26:13 +0900

    presetId로 PresetEcho 객체들을 가져오는 getAllByPresetId()추가

--- src/main/java/com/sjb/wuwaechorank/dao/entity/presetecho/PresetEchoDaoCore.java
+++ src/main/java/com/sjb/wuwaechorank/dao/entity/presetecho/PresetEchoDaoCore.java
@@ -1,8 +1,12 @@
 package com.sjb.wuwaechorank.dao.entity.presetecho;
 
+import java.util.List;
+
 import com.sjb.wuwaechorank.customannotation.DaoCoreInterface;
+import com.sjb.wuwaechorank.entity.PresetEcho;
 
 @DaoCoreInterface
 public interface PresetEchoDaoCore {
-    
+    // presetId로 PresetEcho들을 가져오는 함수
+    List<PresetEcho> getAllByPresetId(int presetId);
 }
