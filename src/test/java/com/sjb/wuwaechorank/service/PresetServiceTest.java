e9c256fa277feb0298806f3e9e16aeb343d69c7e
honeydog3115 <tjs91002256@gmail.com>
2026-08-13 09:24:26 +0900

    add: PresetService.getSimpleResetInfo() 테스트 추가

--- src/test/java/com/sjb/wuwaechorank/service/PresetServiceTest.java
+++ src/test/java/com/sjb/wuwaechorank/service/PresetServiceTest.java
@@ -21,6 +21,7 @@ import com.sjb.wuwaechorank.dao.entity.resonatorecho.ResonatorEchoDao;
 import com.sjb.wuwaechorank.dto.PresetInfoDto;
 import com.sjb.wuwaechorank.dto.ResonatorEchoInfoDto;
 import com.sjb.wuwaechorank.dto.ResonatorEchoSubStatDto;
+import com.sjb.wuwaechorank.dto.SimplePresetInfoDto;
 import com.sjb.wuwaechorank.entity.Preset;
 import com.sjb.wuwaechorank.entity.ResonatorEcho;
 import com.sjb.wuwaechorank.service.preset.PresetService;
@@ -48,6 +49,9 @@ public class PresetServiceTest {
     List<ResonatorEchoSubStatDto> resonatorEchoSubStatDtos;
 
     Preset preset1;
+    Preset preset2;
+    Preset preset3;
+
     ResonatorEcho resonatorEcho1;
     
     @BeforeEach
@@ -58,10 +62,12 @@ public class PresetServiceTest {
             ResonatorEchoSubStatDto.builder().subStatId(3).value("30%").build(),
             ResonatorEchoSubStatDto.builder().subStatId(4).value("40%").build()
         );
+
         this.resonatorEchoInfoDto = List.of(ResonatorEchoInfoDto.builder()
                 .echoId(1)
                 .echoSubStats(this.resonatorEchoSubStatDtos)
                 .build());
+        
         this.presetInfoDto1 = PresetInfoDto.builder()
                 .userId(1)
                 .name("방랑자 프리셋")
@@ -69,7 +75,11 @@ public class PresetServiceTest {
                 .echosInfo(this.resonatorEchoInfoDto)
                 .score(50)
                 .build();
+
         this.preset1 = new Preset(1, "방랑자 프리셋", 1, false, 1, 50.0);
+        this.preset2 = new Preset(1, "에이메스 프리셋", 1, false, 2, 52.0);
+        this.preset3 = new Preset(1, "카르티시아 프리셋", 1, false, 3, 53.0);
+
         this.resonatorEcho1 = ResonatorEcho.builder()
                 .echoId(this.resonatorEchoInfoDto.get(0).echoId())
                 .SubStatId1(resonatorEchoSubStatDtos.get(0).subStatId())
@@ -96,6 +106,17 @@ public class PresetServiceTest {
 
     @Test
     void getSimplePresetInfo(){
+        List<Preset> presets = List.of(this.preset1, this.preset2, this.preset3);
+        when(this.presetDao.getAllByUserId(1)).thenReturn(presets);
 
+        List<SimplePresetInfoDto> simplePresetInfos = presets.stream()
+                .map(preset->SimplePresetInfoDto.builder()
+                        .id(preset.getId())
+                        .name(preset.getName())
+                        .bookmark(preset.getBookmark())
+                        .build())
+                .toList();
+
+        assertThat(this.presetService.getSimplePresetInfo(1)).usingRecursiveComparison().isEqualTo(simplePresetInfos);
     }
 }
