bed4a03b6a5ab3d05b25c8629da7a8209b6a33ff
honeydog3115 <tjs91002256@gmail.com>
2026-08-13 10:20:43 +0900

    add: presetId로 PresetEcho 객체들을 가져오는 getAllByPresetId() 작성

--- src/main/java/com/sjb/wuwaechorank/dao/entity/presetecho/PresetEchoDaoCoreJDBC.java
+++ src/main/java/com/sjb/wuwaechorank/dao/entity/presetecho/PresetEchoDaoCoreJDBC.java
@@ -1,7 +1,25 @@
 package com.sjb.wuwaechorank.dao.entity.presetecho;
 
+
+import java.util.List;
+
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+
+import com.sjb.wuwaechorank.entity.PresetEcho;
 
 @Repository
 public class PresetEchoDaoCoreJDBC implements PresetEchoDaoCore{
+    private final JdbcTemplate jdbcTemplate;
+    private RowMapper<PresetEcho> rowMapper = BeanPropertyRowMapper.newInstance(PresetEcho.class);
+
+    public PresetEchoDaoCoreJDBC(JdbcTemplate jdbcTemplate){
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<PresetEcho> getAllByPresetId(int presetId){
+        return this.jdbcTemplate.query("SELECT * FROM presetecho WHERE presetid = ?", rowMapper, presetId);
+    }
 }
