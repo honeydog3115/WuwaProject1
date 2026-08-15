e56b3e4259df363384823c4f38f9282c84c22e8e
honeydog3115 <tjs91002256@gmail.com>
2026-08-15 19:52:05 +0900

    add: 공명자 상세 정보, 공명자 선택시 필요한 필터 정보를 반환하는 getMethodName, getResonatorFilter 추가

--- src/main/java/com/sjb/wuwaechorank/controller/ResonatorController.java
+++ src/main/java/com/sjb/wuwaechorank/controller/ResonatorController.java
@@ -3,10 +3,15 @@ package com.sjb.wuwaechorank.controller;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sjb.wuwaechorank.dto.ResonatorDetailDto;
+import com.sjb.wuwaechorank.dto.ResonatorFilterDto;
 import com.sjb.wuwaechorank.dto.ResonatorsInfoDto;
 import com.sjb.wuwaechorank.service.resonator.ResonatorService;
+
+
 
 
 @RestController
@@ -18,8 +23,18 @@ public class ResonatorController {
     }
 
     @GetMapping("/resonator")
-    public List<ResonatorsInfoDto> getMethodName() {
+    public List<ResonatorsInfoDto> getResonators() {
         return resonatorService.getAllResonatorInfo();
+    }
+    
+    @GetMapping("/resonator/{id}")
+    public ResonatorDetailDto getMethodName(@PathVariable int id) {
+        return resonatorService.getResonatorDetail(id);
+    }
+    
+    @GetMapping("/resonator/filter")
+    public ResonatorFilterDto getResonatorFilter() {
+        return resonatorService.getResonatorFilter();
     }
     
     
