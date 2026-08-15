276fcc6dc649ceb9a63357481ee2442064e93536
honeydog3115 <tjs91002256@gmail.com>
2026-08-15 19:05:36 +0900

    add: ControllerTest 용 유틸 클래스와 리스트 타입 응답을 검증하는 메서드 ㅜㅊ가

---
+++ src/test/java/com/sjb/wuwaechorank/util/ControllerTestUtil.java
@@ -0,0 +1,40 @@
+package com.sjb.wuwaechorank.util;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.List;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.test.web.servlet.MockMvc;
+
+import tools.jackson.databind.ObjectMapper;
+import tools.jackson.databind.type.CollectionType;
+
+@Component
+public class ControllerTestUtil {
+    private ObjectMapper objectMapper;
+
+    public ControllerTestUtil(ObjectMapper objectMapper){
+        this.objectMapper = objectMapper;
+    }
+
+    public <T> void validateListTypeResponse(MockMvc mockMvc, String uri, Class<T> listType ,List<T> expected){
+        try {
+            String content = mockMvc.perform(get(uri).accept(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isOk())
+                    .andReturn()
+                    .getResponse()
+                    .getContentAsString();
+
+            CollectionType type = objectMapper.getTypeFactory().constructCollectionType(List.class, listType);
+            List<T> response = objectMapper.readValue(content, type);
+
+            assertThat(response).usingRecursiveComparison().isEqualTo(expected);
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
+}
