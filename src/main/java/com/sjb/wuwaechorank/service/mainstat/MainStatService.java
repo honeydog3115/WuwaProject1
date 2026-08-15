ed8038244ff6e431cdbe3795df75aa613985c0da
honeydog3115 <tjs91002256@gmail.com>
2026-08-15 20:07:26 +0900

    주석 추가

--- src/main/java/com/sjb/wuwaechorank/service/mainstat/MainStatService.java
+++ src/main/java/com/sjb/wuwaechorank/service/mainstat/MainStatService.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import com.sjb.wuwaechorank.entity.MainStat;
 
+// 주음속성 서비스 인터페이스
 public interface MainStatService {
+    // 모든 주음속성 정보 반환
     List<MainStat> getAllMainStats();
 }
