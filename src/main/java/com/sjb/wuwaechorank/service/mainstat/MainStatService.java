413ec08ecf9dff3308d9f9534ba95edf71b25e8a
honeydog3115 <tjs91002256@gmail.com>
2026-08-15 20:06:56 +0900

    add: 주음속성 서비스 인터페이스 작성

---
+++ src/main/java/com/sjb/wuwaechorank/service/mainstat/MainStatService.java
@@ -0,0 +1,9 @@
+package com.sjb.wuwaechorank.service.mainstat;
+
+import java.util.List;
+
+import com.sjb.wuwaechorank.entity.MainStat;
+
+public interface MainStatService {
+    List<MainStat> getAllMainStats();
+}
