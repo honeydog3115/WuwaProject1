2dd282c121d8be1f832f5cd0c9be977a567041b6
honeydog3115 <tjs91002256@gmail.com>
2026-08-16 20:23:59 +0900

    update: 에코 계산 부분이 바뀌면서 대응하여 수정

--- src/main/java/com/sjb/wuwaechorank/service/preset/PresetServiceImpl.java
+++ src/main/java/com/sjb/wuwaechorank/service/preset/PresetServiceImpl.java
@@ -4,52 +4,31 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
-import com.sjb.wuwaechorank.dao.entity.echo.EchoDao;
-import com.sjb.wuwaechorank.dao.entity.echosubstatinfo.EchoSubStatInfoDao;
-import com.sjb.wuwaechorank.dao.entity.mainstat.MainStatDao;
 import com.sjb.wuwaechorank.dao.entity.preset.PresetDao;
-import com.sjb.wuwaechorank.dao.entity.resonatorecho.ResonatorEchoDao;
-import com.sjb.wuwaechorank.dao.entity.substat.SubStatDao;
-import com.sjb.wuwaechorank.dao.entity.substatinfo.SubStatInfoDao;
 import com.sjb.wuwaechorank.dto.EchoDetailDto;
 import com.sjb.wuwaechorank.dto.PresetInputInfoDto;
 import com.sjb.wuwaechorank.dto.PresetOutputInfoDto;
 import com.sjb.wuwaechorank.dto.ResonatorEchoInfoDto;
 import com.sjb.wuwaechorank.dto.SimplePresetInfoDto;
-import com.sjb.wuwaechorank.dto.SubStatDetailDto;
-import com.sjb.wuwaechorank.entity.Echo;
-import com.sjb.wuwaechorank.entity.MainStat;
 import com.sjb.wuwaechorank.entity.Preset;
 import com.sjb.wuwaechorank.entity.ResonatorEcho;
 import com.sjb.wuwaechorank.service.resonator.ResonatorService;
+import com.sjb.wuwaechorank.service.resonatorecho.EchoScoreService;
 import com.sjb.wuwaechorank.service.resonatorecho.ResonatorEchoService;
 
 // PresetService 인터페이스 구현체
 @Service
 public class PresetServiceImpl implements PresetService {
     private PresetDao presetDao;
-    private ResonatorEchoDao resonatorEchoDao;
     private ResonatorService resonatorService;
     private ResonatorEchoService resonatorEchoService;
-    private EchoDao echoDao;
-    private MainStatDao mainStatDao;
-    private SubStatDao subStatDao;
-    private SubStatInfoDao subStatInfoDao;
-    private EchoSubStatInfoDao echoSubStatInfoDao;
+    private EchoScoreService echoScoreService;
 
-    public PresetServiceImpl(PresetDao presetDao, ResonatorEchoDao resonatorEchoDao,
-            ResonatorService resonatorService, ResonatorEchoService resonatorEchoService, EchoDao echoDao,
-            MainStatDao mainStatDao, SubStatDao subStatDao, SubStatInfoDao subStatInfoDao,
-            EchoSubStatInfoDao echoSubStatInfoDao) {
+    public PresetServiceImpl(PresetDao presetDao, ResonatorService resonatorService, EchoScoreService echoScoreService, ResonatorEchoService resonatorEchoService) {
         this.presetDao = presetDao;
-        this.resonatorEchoDao = resonatorEchoDao;
         this.resonatorService = resonatorService;
-        this.resonatorEchoDao = resonatorEchoDao;
-        this.echoDao = echoDao;
-        this.mainStatDao = mainStatDao;
-        this.subStatDao = subStatDao;
-        this.subStatInfoDao = subStatInfoDao;
-        this.echoSubStatInfoDao = echoSubStatInfoDao;
+        this.resonatorEchoService = resonatorEchoService;
+        this.echoScoreService = echoScoreService;
     }
 
     @Override
@@ -63,7 +42,7 @@ public class PresetServiceImpl implements PresetService {
                 .bookmark(presetInfoDto.bookmark())
                 .build());
 
-        resonatorEchoService.getResonatorEchoScore(presetInfoDto.resonatorId(), resonatorEchoInfos, true,
+        this.echoScoreService.getResonatorEchoScore(presetInfoDto.resonatorId(), resonatorEchoInfos, true,
                 presetId);
 
         return presetId;
@@ -96,9 +75,9 @@ public class PresetServiceImpl implements PresetService {
     @Override
     public PresetOutputInfoDto getPresetInfo(int presestId) {
         Preset preset = this.presetDao.get(presestId);
-        List<ResonatorEcho> resonatorEchos = resonatorEchoDao.getAllByPresetId(presestId);
+        List<ResonatorEcho> resonatorEchos = this.resonatorEchoService.getResonatorEchosByPresetId(presestId);
         List<EchoDetailDto> echoDetailDtos = resonatorEchos.stream()
-                .map(this::getEchoDetail)
+                .map(resonatorEcho->this.resonatorEchoService.getEchoDetail(resonatorEcho))
                 .toList();
 
         return PresetOutputInfoDto.builder()
@@ -107,25 +86,6 @@ public class PresetServiceImpl implements PresetService {
                 .resonatorDetailDto(resonatorService.getResonatorDetail(preset.getResonatorId()))
                 .echoDetailDtos(echoDetailDtos)
                 .totalScore(preset.getEchoTotalScore())
-                .build();
-    }
-
-    // 에코들의 정보(에코 엔티티, 메인 속성, 부음 속성들)을 가져옴
-    private EchoDetailDto getEchoDetail(ResonatorEcho resonatorEcho) {
-        Echo echo = this.echoDao.get(resonatorEcho.getEchoId());
-        MainStat mainstat = this.mainStatDao.get(resonatorEcho.getMainStatId());
-        List<Integer> echoSubStatInfoIds = this.echoSubStatInfoDao.getIdsByResonatorEchoId(resonatorEcho.getId());
-        List<SubStatDetailDto> subStatDetailDtos = this.subStatInfoDao.getAllByEchoSubStatInfos(echoSubStatInfoIds).stream()
-                .map(subStatInfo -> SubStatDetailDto.builder()
-                        .subStatName(this.subStatDao.get(subStatInfo.getSubStatId()).getName())
-                        .subStatValue(subStatInfo.getValue())
-                        .subStatChance(subStatInfo.getChance())
-                        .build())
-                .toList();
-        return EchoDetailDto.builder()
-                .echo(echo)
-                .mainstat(mainstat)
-                .subStatDetailDtos(subStatDetailDtos)
                 .build();
     }
 }
