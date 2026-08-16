30d279b508aaab33ffb4b8da5f03c7bab7474af3
honeydog3115 <tjs91002256@gmail.com>
2026-08-16 18:44:29 +0900

    SubStatService에 SubStatDetailDto를 반환하는 3가지 함수 추가. 각 substatInfoId, substatInfoids, resonatorEchoId로 가능

--- src/main/java/com/sjb/wuwaechorank/service/substat/SubStatService.java
+++ src/main/java/com/sjb/wuwaechorank/service/substat/SubStatService.java
@@ -2,9 +2,18 @@ package com.sjb.wuwaechorank.service.substat;
 
 import java.util.List;
 
+import com.sjb.wuwaechorank.dto.SubStatDetailDto;
 import com.sjb.wuwaechorank.dto.SubStatWithSubStatInfoDto;
 
 // 부음속성 관련    서비스 인터페이스
 public interface SubStatService {
+    // 모든 서브 스탯을 대상이 갖을 수 있는 값, 확률 전부를 반환하는 함수
     List<SubStatWithSubStatInfoDto> getAllSubStatsWithSubStatInfo();
+    // 서브 스탯의 이름, 값, 확률을 반환하는 함수
+    SubStatDetailDto getSubStatDetail(int subStatInfoId);
+    // 서브 스탯의 이름, 값, 확률이 담긴 객체를 리스트로 반환하는 함수
+    List<SubStatDetailDto> getSubStatDetails(List<Integer> substatInfoIds);
+    // resonatorEchoId로 SubStatDetailDto를 반환하는 함수
+    List<SubStatDetailDto> getSubStatDetailsByResonatorEchoId(int resonatorEchoId);
+
 }
--- src/main/java/com/sjb/wuwaechorank/service/substat/SubStatServiceImpl.java
+++ src/main/java/com/sjb/wuwaechorank/service/substat/SubStatServiceImpl.java
@@ -6,8 +6,10 @@ import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
+import com.sjb.wuwaechorank.dao.entity.echosubstatinfo.EchoSubStatInfoDao;
 import com.sjb.wuwaechorank.dao.entity.substat.SubStatDao;
 import com.sjb.wuwaechorank.dao.entity.substatinfo.SubStatInfoDao;
+import com.sjb.wuwaechorank.dto.SubStatDetailDto;
 import com.sjb.wuwaechorank.dto.SubStatWithSubStatInfoDto;
 import com.sjb.wuwaechorank.entity.SubStat;
 import com.sjb.wuwaechorank.entity.SubStatInfo;
@@ -17,10 +19,12 @@ import com.sjb.wuwaechorank.entity.SubStatInfo;
 public class SubStatServiceImpl implements SubStatService{
     private SubStatDao subStatDao;
     private SubStatInfoDao subStatInfoDao;
+    private EchoSubStatInfoDao echoSubStatInfoDao;
 
-    public SubStatServiceImpl(SubStatDao subStatDao, SubStatInfoDao subStatInfoDao){
+    public SubStatServiceImpl(SubStatDao subStatDao, SubStatInfoDao subStatInfoDao, EchoSubStatInfoDao echoSubStatInfoDao){
         this.subStatDao = subStatDao;
         this.subStatInfoDao = subStatInfoDao;
+        this.echoSubStatInfoDao = echoSubStatInfoDao;
     }
 
     // 에코의 부음속성 선택시 필요한 모든 부음속성 정보들을 반환.
@@ -38,5 +42,26 @@ public class SubStatServiceImpl implements SubStatService{
                         .subStatInfos(subStatInfoGroupBySubStat.getOrDefault(subStat.getId(), List.of()))
                         .build())
                 .toList();
+    }
+
+    @Override
+    public SubStatDetailDto getSubStatDetail(int subStatInfoId) {
+        SubStatInfo subStatInfo = this.subStatInfoDao.get(subStatInfoId);
+        SubStat subStat = this.subStatDao.get(subStatInfo.getSubStatId());
+        return SubStatDetailDto.builder()
+                .subStatName(subStat.getName())
+                .subStatValue(subStatInfo.getValue())
+                .subStatChance(subStatInfo.getChance())
+                .build();
+    }
+
+    @Override
+    public List<SubStatDetailDto> getSubStatDetails(List<Integer> substatInfoIds) {
+        return this.subStatDao.getSubStatDetailsBysubStatInfoIds(substatInfoIds);
+    }
+
+    @Override
+    public List<SubStatDetailDto> getSubStatDetailsByResonatorEchoId(int resonatorEchoId) {
+        return this.getSubStatDetails(this.echoSubStatInfoDao.getSubStatInfoIdsByResonatorEchoId(resonatorEchoId));
     }
 }
