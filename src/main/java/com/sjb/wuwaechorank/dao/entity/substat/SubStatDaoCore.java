2820164238af9185ca8c22412149d0007c03476e
honeydog3115 <tjs91002256@gmail.com>
2026-08-16 18:16:33 +0900

    add: id들로 substat들을 반환하는 SubStatDaoCore.getByIds() 작성

--- src/main/java/com/sjb/wuwaechorank/dao/entity/substat/SubStatDaoCore.java
+++ src/main/java/com/sjb/wuwaechorank/dao/entity/substat/SubStatDaoCore.java
@@ -1,8 +1,12 @@
 package com.sjb.wuwaechorank.dao.entity.substat;
 
+import java.util.List;
+
 import com.sjb.wuwaechorank.customannotation.DaoCoreInterface;
+import com.sjb.wuwaechorank.entity.SubStat;
 
 @DaoCoreInterface
 public interface SubStatDaoCore {
-    
+    // id들로 substat들을 가져오는 함수
+    List<SubStat> getByIds(List<Integer> ids);
 }
--- src/main/java/com/sjb/wuwaechorank/dao/entity/substat/SubStatDaoCoreJDBC.java
+++ src/main/java/com/sjb/wuwaechorank/dao/entity/substat/SubStatDaoCoreJDBC.java
@@ -1,8 +1,37 @@
 package com.sjb.wuwaechorank.dao.entity.substat;
 
+import java.util.List;
+
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+import com.sjb.wuwaechorank.entity.SubStat;
 
 @Repository
 public class SubStatDaoCoreJDBC implements SubStatDaoCore {
+    private JdbcTemplate jdbcTemplate;
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+    private RowMapper<SubStat> rowMapper = BeanPropertyRowMapper.newInstance(SubStat.class); 
+
+
+    public SubStatDaoCoreJDBC(JdbcTemplate jdbcTemplate, NamedParameterJdbcTemplate namedParameterJdbcTemplate){
+        this.jdbcTemplate = jdbcTemplate;
+        this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
+    }
+
+    @Override
+    public List<SubStat> getByIds(List<Integer> ids) {
+        if(ids == null || ids.isEmpty())
+            return List.of();
+
+        MapSqlParameterSource parameter = new MapSqlParameterSource();
+        parameter.addValue("ids", ids);
+        
+        return this.namedParameterJdbcTemplate.query("SELECT * FROM substat WHERE id IN (:ids)", parameter, rowMapper);
+    }
     
 }
