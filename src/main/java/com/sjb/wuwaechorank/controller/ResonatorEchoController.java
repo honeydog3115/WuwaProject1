089e9df0df3092bac56f2190224f71acd24f87d6
honeydog3115 <tjs91002256@gmail.com>
2026-08-16 15:49:27 +0900

    add: ResonatorEchoController 클래스 작성, 공명자 에코 점수를 반환하는 POST getResonatorEchoScore() 추가

--- src/main/java/com/sjb/wuwaechorank/controller/ResonatorEchoController.java
+++ src/main/java/com/sjb/wuwaechorank/controller/ResonatorEchoController.java
@@ -1,30 +1,26 @@
 package com.sjb.wuwaechorank.controller;
 
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.sjb.wuwaechorank.service.resonator.ResonatorService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-
-
+import com.sjb.wuwaechorank.dto.request.ResonatorEchoScoreRequest;
+import com.sjb.wuwaechorank.service.resonatorecho.ResonatorEchoService;
 
 @RestController
 public class ResonatorEchoController {
-    private ResonatorService resonatorService;
+    private ResonatorEchoService resonatorEchoService;
 
-    public ResonatorEchoController(ResonatorService resonatorService){
-        this.resonatorService = resonatorService;
+    public ResonatorEchoController(ResonatorEchoService resonatorEchoService){
+        this.resonatorEchoService = resonatorEchoService;
     }
 
     @PostMapping("path")
-    public String postMethodName(@RequestBody String entity) {
-        //TODO: process POST request
-        
-        return entity;
+    public double getResonatorEchoScore(@RequestBody ResonatorEchoScoreRequest request) {
+        return resonatorEchoService.getResonatorEchoScore(
+            request.id(), 
+            request.resonatorEchoInfoDtos(), 
+            request.insertDB(), 
+            request.presetId());
     }
 }
